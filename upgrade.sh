fdbcb0aa1d89b4b0a717f42d965ce6b6b321a64e
istiak101 <30789544+istiak101@users.noreply.github.com>
2022-05-03 18:18:58 +0600

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,9 @@
 rm -f /root/gitea
 rm -f /usr/local/bin/gitea
+systemctl stop gitea
 
 version=1.16.7
 wget -O gitea https://dl.gitea.io/gitea/$version/gitea-$version-linux-amd64
 chmod +x gitea
 cp gitea /usr/local/bin/gitea
+systemctl start gitea
